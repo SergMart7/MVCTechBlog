035deaf20051016e3923b4093c7d367975de62f4
Sergio Martinez <sergmart7@Sergios-MacBook-Air.local>
2024-10-03 21:20:02 -0600

    render fix

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
-const userRoutes = require("./userRoutes");
+const path = require('path');
+
+// Using absolute paths to require route modules
+const userRoutes = require(path.join(__dirname, 'userRoutes'));
 const postRoutes = require("./postRoutes");
 const commentRoutes = require("./commentRoutes");
-
 
 router.use("/users", userRoutes);
 router.use("/posts", postRoutes);
