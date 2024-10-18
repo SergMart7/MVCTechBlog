c8adc515944d00edf4841c131bbe6ce704df86af
Sergio Martinez <sergmart7@Sergios-MacBook-Air.local>
2024-10-17 22:20:14 -0600

    aaaaaaf

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,6 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-if (!process.env.DATABASE_URL) {
-  throw new Error('DATABASE_URL environment variable is missing!');
-}
-
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   protocol: 'postgres',
