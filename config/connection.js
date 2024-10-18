4a493ce139fc219ffd98bc3329eb5fab72e7c6bd
Sergio Martinez <sergmart7@Sergios-MacBook-Air.local>
2024-10-17 18:00:31 -0600

    render

--- config/connection.js
+++ config/connection.js
@@ -4,20 +4,20 @@ require('dotenv').config();
 let sequelize;
 
 if (process.env.DB_URL) {
-  // For production environment using DATABASE URL (e.g., from Render)
+  // For production environment using Render's database URL
   sequelize = new Sequelize(process.env.DB_URL, {
     dialect: 'postgres',
     protocol: 'postgres',
-    logging: false, // Disable logging for production, enable if needed
     dialectOptions: {
       ssl: {
-        require: true, // Enforce SSL for secure connection in production
-        rejectUnauthorized: false // Required for Render's self-signed SSL certs
+        require: true,
+        rejectUnauthorized: false
       }
-    }
+    },
+    logging: false
   });
 } else {
-  // For local development environment
+ 
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
@@ -26,7 +26,7 @@ if (process.env.DB_URL) {
       host: process.env.DB_HOST || 'localhost',
       dialect: 'postgres',
       port: process.env.DB_PORT || 5432,
-      logging: false 
+      logging: false
     }
   );
 }
