a2e034c8df95c15cd3047dcc9d913fa093519d43
Sergio Martinez <sergmart7@Sergios-MacBook-Air.local>
2024-10-17 21:40:27 -0600

    heeeeelpp

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,5 @@
-require('dotenv').config();
 const Sequelize = require('sequelize');
+require('dotenv').config();
 
 const sequelize = process.env.DATABASE_URL
   ? new Sequelize(process.env.DATABASE_URL, {
@@ -13,9 +13,9 @@ const sequelize = process.env.DATABASE_URL
       },
     })
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306,
+      host: process.env.DB_HOST,
+      dialect: 'postgres',
+      port: process.env.DB_PORT || 5432,
     });
 
 module.exports = sequelize;
