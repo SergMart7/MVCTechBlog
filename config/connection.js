feb9613b291a007784981d1736856c510dcc5b90
Sergio Martinez <sergmart7@Sergios-MacBook-Air.local>
2024-10-17 22:10:05 -0600

    pwease just pwease

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,17 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL environment variable is missing!');
+}
+
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   protocol: 'postgres',
   dialectOptions: {
     ssl: {
-      require: true,           
-      rejectUnauthorized: false 
+      require: true,
+      rejectUnauthorized: false,
     },
   },
 });
