76052c45d22013466c228cfd26474a7322185cbd
Sergio Martinez <sergmart7@Sergios-MacBook-Air.local>
2024-10-17 22:01:08 -0600

    pwease

--- config/connection.js
+++ config/connection.js
@@ -7,15 +7,15 @@ const sequelize = process.env.DATABASE_URL
       protocol: 'postgres',
       dialectOptions: {
         ssl: {
-          require: true,
-          rejectUnauthorized: false,
+          require: true,         
+          rejectUnauthorized: false
         },
       },
     })
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: process.env.DB_HOST,
+      host: process.env.DB_HOST, 
+      port: process.env.DB_PORT || 5432, 
       dialect: 'postgres',
-      port: process.env.DB_PORT || 5432,
     });
 
 module.exports = sequelize;
